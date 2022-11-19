b269ed4c50263d6948a1563da32562f856c7ab72
Zaiyd Mala <52519982+zaiydMala@users.noreply.github.com>
2022-11-20 01:36:46 +0530

    Draw a new card

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-let firstCard = 0;
-let secondCard = 0;
+let firstCard = 10;
+let secondCard = 5;
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
 let message = "";
-
+let messageEl = document.querySelector('.message-el');
+let cardEl = document.querySelector('.card-el');
+let sumEl = document.querySelector('.sum-el');
 const start = document.querySelector('.start');
-start.addEventListener('click', startGame());
+const newCard = document.querySelector('.new-card');
 
-function startGame() {
+
+start.addEventListener('click', startGame);
+function startGame() { playRound() }
+function playRound() {
+    cardEl.textContent = `Cards: ${firstCard}  ${secondCard}`;
+    sumEl.textContent = `Sum: ${sum}`;
+
     if(sum <= 20) {
         message = "do you wish to draw a new card? 🙂";
     } else if(sum === 21) {
@@ -18,6 +26,13 @@ function startGame() {
         message = "you're out of the game 😭";
         isAlive = false;
     }
+    messageEl.textContent = message;
 }
 
+newCard.addEventListener('click', drawCard);
+function drawCard() {
+    const card = 6;
+    sum += card;
+    playRound();
+}
 
