c57983e67fa7c02ebbb783d2e10c21bab0f1809f
Zaiyd Mala <52519982+zaiydMala@users.noreply.github.com>
2022-11-20 00:09:56 +0530

    Store last game state in a variable

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 let firstCard = 0;
 let secondCard = 0;
-
 let sum = firstCard + secondCard;
+let hasBlackjack = false;
+let isAlive = true;
+let message = "";
 
-if(sum < 21) {
-    console.log("do you wish to draw a new card? 🙂");
+if(sum <= 20) {
+    message = "do you wish to draw a new card? 🙂";
+} else if(sum === 21) {
+    message = "you win 🥳";
+    hasBlackjack = true;
+} else {
+    message = "you're out of the game 😭";
+    isAlive = false;
 }
-else if(sum === 21) {
-    console.log("you win 🥳");
-}
-
-else if(sum > 21) {
-    console.log("you're out of the game 😭");
-}
-
 
 
